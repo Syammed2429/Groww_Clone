1bb4d8b553b4c6a0a0ace7c53923c2c7bf1fe4b0
Dada Khalandar <kerry.blig12@gmail.com>
2021-08-27 22:27:09 +0530

    wallet

--- Groww_Clone/dashboard.js
+++ Groww_Clone/dashboard.js
@@ -1,43 +1,81 @@
-let userMenu = document.querySelector('.userIcon');
-    const menuDiv = document.createElement('div');
-    
-    function dropDownMenu() {
-        const name = document.createElement('div');
-        const email = document.createElement('div');
-        const importFunds = document.createElement('div');
-        const watchList = document.createElement('div');
-        const orders = document.createElement('div');
-        const help = document.createElement('div');
-        const logOut = document.createElement('div');
-        let count = 0;
 
+//Declearing the variables
+let userMenu = document.querySelector('.userIcon');
+const menuDiv = document.createElement('div');
+let darkMode = document.createElement('div');
+const name = document.createElement('div');
+const email = document.createElement('div');
+const importFunds = document.createElement('div');
+const watchList = document.createElement('div');
+const orders = document.createElement('div');
+const help = document.createElement('div');
+const logOut = document.createElement('div');
+const close = document.createElement('span');
+
+//creting classes for the above variables
+menuDiv.classList.add('menu')
+darkMode.classList.add('darkModeOn')
+importFunds.classList.add('importFunds')
+
+
+
+    function dropDownMenu() {
+        
+        let count = 0;
         importFunds.innerHTML = "Import Funds";
         watchList.innerHTML = "Watch List";
         orders.innerHTML = "Order";
+        darkMode.innerHTML = "Dark Mode";
         logOut.innerHTML = "Log Out";
-        menuDiv.append(importFunds,watchList,orders,logOut);
-        menuDiv.classList.add('content')
+        close.innerHTML = "close&times;";
+
+        //Cretaing a class for menuDiv
+        
+        menuDiv.classList.add('content');
+
+        //appending all the items to the menuDiv
+
+        menuDiv.append(close,importFunds,watchList,orders,darkMode,logOut);
+
+        //appending the items of the menuDiv to the userMenu
         userMenu.append(menuDiv);
+
+        close.onclick = function() {
+            menuDiv.style.display = "none";
+          }
+
         count++;
 
         if (count >= 1) {
             userMenu.removeEventListener('click', dropDownMenu);
         }
+        
     }
+    // c.classList.add('baby')
 
-    function dropMenuDissaper () {
-        console.log(userMenu);
-        menuDiv.innerHTML = '';
-    }
 
     
-    function darkMode() {
-        let darkMode = document.body;
-        darkMode.classList.toggle('dark-mode')
-        
+    function darkModeToggle() {
+        let darkModeButton = document.querySelector('.darkModeOn');
+        let a = document.querySelector('.stock-name')
+        let b = document.querySelector('.menu')
+        a.style.color = 'white'
+        b.style.color = 'black'
+        darkModeButton = document.body;
+         darkModeButton.classList.toggle('dark-mode');
 
-    }
+        }
 
+        function addMoney () {
+            // let importFund = querySelector('.importFunds');
+            console.log('importFund:')
+            window.location = 'import-funds.html';
+            // console.log('importFund:')
 
-    userMenu.addEventListener('dblclick', dropMenuDissaper);
+            // window.location('import-funds.html')
+        }
+
+    // userMenu.addEventListener('dblclick', dropMenuDissaper);
+    importFunds.addEventListener('click', addMoney)
     userMenu.addEventListener('click', dropDownMenu);
+    darkMode.addEventListener('click',darkModeToggle)
